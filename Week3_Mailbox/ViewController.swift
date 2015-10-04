6cb783da41297bcd8363d624e8c1c5b00c52462a
yeahnoah <noah@noahbatterson.com>
2015-10-04 16:23:55 -0700

    Initial Commit

---
+++ Week3_Mailbox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week3_Mailbox
+//
+//  Created by Noah Batterson on 10/4/15.
+//  Copyright © 2015 Noah Batterson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
